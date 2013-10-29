dade2975c196e91061d2829eb52b4d37e5d2359e
tarish <tarish@gmail.com>
2013-10-29 03:09:48 -0400

    Added bouncy icons to interests page and centered all links. Definitely need to clean up CSS file. Too much duplication on list styles on every page of site.

--- public/javascript/helloWorld.js
+++ public/javascript/helloWorld.js
@@ -1,3 +1,10 @@
-function hello() {
-	alert("Hello JavaScript!");
+function showIcons() {
+	$("#networks a").hover(function() {
+		$(this).effect("bounce", { distance: 25, times: 1 }, "slow" );
+	});
+}
+
+showIcons();
+
+function hideIcons() {
 }
